850be2d482fd5b08ab695c9d33909e632266a99f
Eric Jones <joneserice@gmail.com>
2013-04-28 02:07:54 -0500

    Updated regex for additional video upload sites

--- app/models/resumevid.rb
+++ app/models/resumevid.rb
@@ -1,8 +1,7 @@
 class Resumevid < ActiveRecord::Base
   belongs_to :member
   attr_accessible :description, :name, :originvid, :rvideo
-  VALID_ORIGINVID_REGEX = /(?:http:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/
+  VALID_ORIGINVID_REGEX = /(?:youtube\.com|youtu\.be)|(?:vimeo\.com)/
   validates :originvid, :presence=>   true, :format=> { with: VALID_ORIGINVID_REGEX }
   validates :name, :presence=> true, :length=> { :maximum=> 100 }
-
 end
