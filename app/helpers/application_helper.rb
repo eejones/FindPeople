7fe918177e16cd3ece3e1b1a78f30654e480f43d
Eric Jones <joneserice@gmail.com>
2013-05-27 02:45:57 -0500

    updates to filter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,19 +3,17 @@ module ApplicationHelper
   def select_tag_for_filter(member, nvpairs, params)
     options = { :query => params[:query] }
     _url = url_for(eval("#{member}_url(options)"))
-    _html = %{<label for="show">Show:</label><br />}
-    _html << %{<select name="show" id="show"}
+    _html = %{%label(for='show') Show:}
+    _html << %{%select :name=>"show" :id=>"show"}
     _html << %{onchange="window.location='#{_url}' + '?show=' + this.value">}
     nvpairs.each do |pair|
-      _html << %{<option value="#{pair[:scope]}"}
+      _html << %{%option(value='#{pair[:scope]}')}
       if params[:show] == pair[:scope] || ((params[:show].nil? || 
   params[:show].empty?) && pair[:scope] == "all")
       _html << %{ selected="selected"}
       end
-      _html << %{>#{pair[:label]}}
-      _html << %{</option>}
+      _html << %{#{pair[:label]}}
     end
-    _html << %{</select>}
   end
 
  def youtube_video(profilevid)
