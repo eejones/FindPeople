c751814a171b03dc21ac160fe292b9f8ac55245e
Eric Jones <joneserice@gmail.com>
2013-09-22 02:12:20 -0500

    adding some changes to groups branch. next step will be transitioning auths to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   include SessionsHelper
-
   include MembersHelper
+
+
+  def this_member
+    @this_member ||= Member.find_by_remember_token(cookies[:remember_token])
+  end
+
+#    @this_member ||= Member.find_by_remember_token(cookies[:remember_token])
+
+  def this_member?(member)
+    member == this_member
+  end
+
+  def is_signed_in?
+    !this_member.nil?
+  end
+
+  helper_method :is_signed_in?
+  helper_method :this_member
 
   # Force signout to prevent CSRF attacks
   def handle_unverified_request
