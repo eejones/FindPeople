be90059ae500dc65f1656c71385aec42eaa387e1
Eric Jones <joneserice@gmail.com>
2013-09-29 01:35:55 -0500

    Grouping setup is fixed now

--- app/controllers/groupings_controller.rb
+++ app/controllers/groupings_controller.rb
@@ -1,16 +1,27 @@
 class GroupingsController < ApplicationController
-  before_action :signed_in_member
+#  before_action :signed_in?
 
   def new
     Grouping.new
   end
 
   def create
-    @grouping=Grouping.new
-    @group = Group.find(params[:grouping][:group_id])
-    @member= Member.find(params[:grouping][:member_id])
-    @group.join!(@member)
-    redirect_to @current_member
+    @group = Group.find_by_id(params[:grouping][:group_id])
+    @member = current_member
+#    @grouping = @member.groupings.create(:group_id => "13")
+    @grouping = @group.groupings.create(:member_id => @member.id)
+    if @grouping.save
+      flash[:notice] = "You have joined this group."
+      redirect_to :back
+    else
+      flash[:error] = "Unable to join."
+      redirect_to :back
+    end
+#    @grouping=Grouping.new
+#    @group = Group.find(params[:grouping][:group_id])
+#    @member= Member.find(params[:grouping][:member_id])
+#    @group.join!(@member)
+#    redirect_to @current_member
   end
 
   def destroy
